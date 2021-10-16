8b7fee30f79d911ba144577b2419041b16e111e5
WesselvD <wesselvandommelen@gmail.com>
2021-10-16 18:00:15 +0200

    init commit

---
+++ src/main/java/com/feedingfrenzy/feedme/SarafeedsApplication.java
@@ -0,0 +1,13 @@
+package com.feedingfrenzy.feedme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarafeedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedingfrenzy/feedme/SarafeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedingfrenzy.feedme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
